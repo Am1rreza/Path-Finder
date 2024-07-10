234de34e1aa3a62e624e2bcd3c547db7b2dcc979
Amirreza <amirrezamousavi9130@gmail.com>
2024-07-10 21:19:16 +0430

    implement the logic of the BFS algorithm

--- path_finder.py
+++ path_finder.py
@@ -23,6 +23,59 @@ def print_maze(maze, stdscr, path=[]):
     for j, value in enumerate(row):
       stdscr.addstr(i, j*2, value, BLUE)
   
+def find_start(maze, start):
+  for i, row in enumerate(maze):
+    for j, value in enumerate(row):
+      if value == start:
+        return i, j
+      
+  return None
+
+def find_path(maze, stdscr):
+  start = "O"
+  end = "X"
+  start_pos = find_start(maze,start)
+
+  q = queue.Queue()
+  q.put((start_pos, [start_pos]))
+
+  visited = set()
+
+  while not q.empty():
+    current_pos, path = q.get()
+    row, col = current_pos
+
+    if maze[row][col] == end:
+      return path
+    
+    neighbors = find_neighbors(maze, row, col)
+    for neighbor in neighbors:
+      if neighbors in visited:
+        continue
+
+      r, c = neighbor
+      if maze[r][c] == "#":
+        continue
+
+      new_path = path + [neighbor]
+      q.put((neighbor, new_path))
+      visited.add(neighbor)
+    
+def find_neighbors(maze, row, col):
+  neighbors = []
+
+  if row > 0: # UP
+    neighbors.append((row - 1, col))
+  if row + 1 < len(maze): # DOWN
+    neighbors.append((row + 1, col))
+  if col > 0: # LEFT
+    neighbors.append((row, col - 1))
+  if col + 1 < len(maze[0]): # RIGHT
+    neighbors.append(row, col + 1)
+
+  return neighbors
+
+
 
 def main(stdscr):
   curses.init_pair(1, curses.COLOR_BLUE,curses.COLOR_BLACK)

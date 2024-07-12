8f77dbcfb86b52840d6dced82b782dfb30341480
Amirreza <amirrezamousavi9130@gmail.com>
2024-07-12 13:36:57 +0430

    refactor and format the app

--- path_finder.py
+++ path_finder.py
@@ -3,7 +3,8 @@ from curses import wrapper
 import queue
 import time
 
-maze = [
+# Define the maze as a global variable
+MAZE = [
     ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
@@ -15,81 +16,84 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
-def print_maze(maze, stdscr, path=[]):
-  BLUE = curses.color_pair(1)
-  RED = curses.color_pair(2)
+# Constants for start and end points
+START = "O"
+END = "X"
 
-  for i, row in enumerate(maze):
-    for j, value in enumerate(row):
-      if (i, j) in path:
-        stdscr.addstr(i, j*2, "X", RED)
-      else:
-        stdscr.addstr(i, j*2, value, BLUE)
-  
-def find_start(maze, start):
-  for i, row in enumerate(maze):
-    for j, value in enumerate(row):
-      if value == start:
-        return i, j
-      
-  return None
+
+def initialize_curses():
+    curses.initscr()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            color = RED if (i, j) in path else BLUE
+            stdscr.addstr(i, j * 2, value if (i, j)
+                          not in path else 'X', color)
+
+
+def find_start(maze, start_symbol):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start_symbol:
+                return i, j
+    return None
+
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # UP, DOWN, LEFT, RIGHT
+
+    for dr, dc in directions:
+        r, c = row + dr, col + dc
+        if 0 <= r < len(maze) and 0 <= c < len(maze[0]):
+            neighbors.append((r, c))
+
+    return neighbors
+
 
 def find_path(maze, stdscr):
-  start = "O"
-  end = "X"
-  start_pos = find_start(maze,start)
+    start_pos = find_start(maze, START)
+    if start_pos is None:
+        return []
 
-  q = queue.Queue()
-  q.put((start_pos, [start_pos]))
+    q = queue.Queue()
+    q.put((start_pos, [start_pos]))
+    visited = set()
 
-  visited = set()
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
 
-  while not q.empty():
-    current_pos, path = q.get()
-    row, col = current_pos
+        stdscr.clear()
+        print_maze(maze, stdscr, path)
+        time.sleep(0.2)
+        stdscr.refresh()
 
-    stdscr.clear()
-    print_maze(maze, stdscr, path)
-    time.sleep(0.2)
-    stdscr.refresh()
+        if maze[row][col] == END:
+            return path
 
-    if maze[row][col] == end:
-      return path
-    
-    neighbors = find_neighbors(maze, row, col)
-    for neighbor in neighbors:
-      if neighbor in visited:
-        continue
+        for neighbor in find_neighbors(maze, row, col):
+            if neighbor not in visited:
+                r, c = neighbor
+                if maze[r][c] != "#":
+                    visited.add(neighbor)
+                    q.put((neighbor, path + [neighbor]))
 
-      r, c = neighbor
-      if maze[r][c] == "#":
-        continue
-
-      new_path = path + [neighbor]
-      q.put((neighbor, new_path))
-      visited.add(neighbor)
-    
-def find_neighbors(maze, row, col):
-  neighbors = []
-
-  if row > 0: # UP
-    neighbors.append((row - 1, col))
-  if row + 1 < len(maze): # DOWN
-    neighbors.append((row + 1, col))
-  if col > 0: # LEFT
-    neighbors.append((row, col - 1))
-  if col + 1 < len(maze[0]): # RIGHT
-    neighbors.append((row, col + 1))
-
-  return neighbors
-
+    return []
 
 
 def main(stdscr):
-  curses.init_pair(1, curses.COLOR_BLUE,curses.COLOR_BLACK)
-  curses.init_pair(2, curses.COLOR_RED,curses.COLOR_BLACK)
+    initialize_curses()
+    path = find_path(MAZE, stdscr)
+    stdscr.getch()
 
-  find_path(maze, stdscr)
-  stdscr.getch()
 
 wrapper(main)
